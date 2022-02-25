a788baf5900c1b6b0b8f9bb9cc61f5a75102cd5e
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2022-02-25 22:36:11 +0100

    feat: initial boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Works");
+import "reflect-metadata";
+import { createExpressServer } from "routing-controllers";
+import controllers from "./controller";
+
+const PORT = 3000;
+
+const app = createExpressServer({
+  controllers,
+});
+
+console.log("server run on localhost:", PORT);
+app.listen(PORT);
---
+++ src/controller/UserController.ts
@@ -0,0 +1,5 @@
+import "reflect-metadata";
+import { JsonController } from "routing-controllers";
+
+@JsonController()
+export class UserController {}
